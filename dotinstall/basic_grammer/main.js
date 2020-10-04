e317c2835af453089af9eb36f75ae86b78ea321b
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-04 15:33:44 +0900

    undefinedについて

--- dotinstall/basic_grammer/main.js
+++ dotinstall/basic_grammer/main.js
@@ -1,12 +1,7 @@
 'use strict';
 
-<<<<<<< HEAD
 let variables;
 var variables2;
 
 console.log(variables);
 console.log(variables2);
-
-console.log(1 + 1);
-=======
->>>>>>> 29ca71c0ec1cee1d59eb2ddd7d2efd892d1bef45
