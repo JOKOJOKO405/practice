d6a13bd71208e1a2eb68dfac81a940e7e002f40e
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-05 10:19:44 +0900

    例外処理

--- dotinstall/object/main.js
+++ dotinstall/object/main.js
@@ -1,30 +1,12 @@
 'use strict';
 
 {
-  // minからmaxまでの整数
-  // Math.floor(Math.random()*(max + 1 - min)) + min
-  // console.log(Math.floor(Math.random()*6) + 1);
+  const name = 17;
 
-  // let i = 0;
-  // function showTime(){
-  //   console.log(new Date());
-  //   i++;
-  //   if(i > 4){
-  //     clearInterval(timerId);
-  //   }
-  // }
-  // const timerId = setInterval(showTime, 1000);
-
-  let i = 0;
-  function timeOut(){
-    console.log(new Date());
-    const timeOutId = setTimeout(timeOut, 1000);
-    i++;
-    if(i > 6){
-      clearTimeout(timeOutId);
-    }
+  try {
+    console.log(name.toUpperCase());  
+  } catch (error) {
+    console.log(error);
   }
-
-  timeOut();
-
+  
 }
