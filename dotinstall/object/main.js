b498acc45f67c638e88c68e31f61324098445ed6
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-05 10:14:38 +0900

    タイマー

--- dotinstall/object/main.js
+++ dotinstall/object/main.js
@@ -1,56 +1,30 @@
 'use strict';
 
-// {
-//   const otherPoint = {
-//     r: 4,
-//     color: 'red',
-//   };
-//   const point = {
-//     x: 120,
-//     y: 100,
-//     ...otherPoint,
-//   }; 
-
-//   console.log(point);
-
-//   // レスト構文
-//   const { x, r, ...others} = point;
-
-//   console.log(x);
-//   console.log(r);
-//   console.log(others);
-
-//   // オブジェクトを配列にする
-//   Object.keys(point).forEach(key => {
-//     console.log(`Keys: ${key} Value: ${point[key]}`);
-//   })
-
-
-//   let x = [1, 2];
-//   let y = [...x];
-//   x[0] = 5;
-//   console.log(x);
-//   console.log(y);
-
-//   const str = ['あいう', 'えおか', 'きくけ'];
-//   console.log(str.join(''));
-//   const t = '12/10/45';
-//   const [h, m, s] = t.split('/');
-//   console.log(h);
-//   console.log(m);
-// }
-
 {
-  const scores = [10, 3, 9];
+  // minからmaxまでの整数
+  // Math.floor(Math.random()*(max + 1 - min)) + min
+  // console.log(Math.floor(Math.random()*6) + 1);
 
-  let sum = 0;
+  // let i = 0;
+  // function showTime(){
+  //   console.log(new Date());
+  //   i++;
+  //   if(i > 4){
+  //     clearInterval(timerId);
+  //   }
+  // }
+  // const timerId = setInterval(showTime, 1000);
 
-  scores.forEach(score => {
-    sum += score;
-  });
+  let i = 0;
+  function timeOut(){
+    console.log(new Date());
+    const timeOutId = setTimeout(timeOut, 1000);
+    i++;
+    if(i > 6){
+      clearTimeout(timeOutId);
+    }
+  }
 
-  const avg = sum / scores.length;
+  timeOut();
 
-  console.log(sum);
-  console.log(Math.floor(avg));
 }
