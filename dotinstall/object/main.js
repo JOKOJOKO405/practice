196e393a8aac160cbd3d1632f91148c5b256515d
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-05 09:43:36 +0900

    数値に関して

--- dotinstall/object/main.js
+++ dotinstall/object/main.js
@@ -1,41 +1,56 @@
 'use strict';
 
+// {
+//   const otherPoint = {
+//     r: 4,
+//     color: 'red',
+//   };
+//   const point = {
+//     x: 120,
+//     y: 100,
+//     ...otherPoint,
+//   }; 
+
+//   console.log(point);
+
+//   // レスト構文
+//   const { x, r, ...others} = point;
+
+//   console.log(x);
+//   console.log(r);
+//   console.log(others);
+
+//   // オブジェクトを配列にする
+//   Object.keys(point).forEach(key => {
+//     console.log(`Keys: ${key} Value: ${point[key]}`);
+//   })
+
+
+//   let x = [1, 2];
+//   let y = [...x];
+//   x[0] = 5;
+//   console.log(x);
+//   console.log(y);
+
+//   const str = ['あいう', 'えおか', 'きくけ'];
+//   console.log(str.join(''));
+//   const t = '12/10/45';
+//   const [h, m, s] = t.split('/');
+//   console.log(h);
+//   console.log(m);
+// }
+
 {
-  const otherPoint = {
-    r: 4,
-    color: 'red',
-  };
-  const point = {
-    x: 120,
-    y: 100,
-    ...otherPoint,
-  }; 
+  const scores = [10, 3, 9];
 
-  console.log(point);
+  let sum = 0;
 
-  // レスト構文
-  const { x, r, ...others} = point;
+  scores.forEach(score => {
+    sum += score;
+  });
 
-  console.log(x);
-  console.log(r);
-  console.log(others);
+  const avg = sum / scores.length;
 
-  // オブジェクトを配列にする
-  Object.keys(point).forEach(key => {
-    console.log(`Keys: ${key} Value: ${point[key]}`);
-  })
-
-  
-  let x = [1, 2];
-  let y = [...x];
-  x[0] = 5;
-  console.log(x);
-  console.log(y);
-
-  const str = ['あいう', 'えおか', 'きくけ'];
-  console.log(str.join(''));
-  const t = '12/10/45';
-  const [h, m, s] = t.split('/');
-  console.log(h);
-  console.log(m);
+  console.log(sum);
+  console.log(Math.floor(avg));
 }
