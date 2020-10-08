cab75310ea1b1f50d48b88cdba388d1e5b81180c
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-09 05:05:27 +0900

    todoアプリファイル作成

---
+++ dotinstall/vue/main.js
@@ -0,0 +1,17 @@
+(function(){
+  'use strict';
+
+  var vn = new Vue({
+    el: '#app',
+    data: {
+      todos: [
+        'task1',
+        'task2',
+        'task3',
+      ],
+    }
+  });
+
+
+})();
+
