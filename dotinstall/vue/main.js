2f24089e96151ddd27c74a48788125097df035f6
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-09 06:02:31 +0900

    todo残数表示まで

--- dotinstall/vue/main.js
+++ dotinstall/vue/main.js
@@ -4,11 +4,41 @@
   var vn = new Vue({
     el: '#app',
     data: {
-      todos: [
-        'task1',
-        'task2',
-        'task3',
-      ],
+      newItem: '',
+      todos: [{
+        title: 'task1',
+        isDone: false,
+      },{
+        title: 'task2',
+        isDone: false,
+      },{
+        title: 'task3',
+        isDone: true,
+      }
+    ]
+    },
+    methods: {
+      addItem: function() {
+        var item = {
+          title: this.newItem,
+          isDone: false,
+        };
+        this.todos.push(item);
+        this.newItem = '';
+      },
+      deleteItem: function(index) {
+        if(confirm('surely?')){
+          this.todos.splice(index, 1);
+        }
+      }
+    },
+    computed: {
+      remaining: function() {
+        var item = this.todos.filter(function(todo){
+          return !todo.isDone;
+        });
+        return item.length;
+      }
     }
   });
 
