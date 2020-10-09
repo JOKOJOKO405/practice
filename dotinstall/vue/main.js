a752516c50998c0793bd294870f3fb828307ccfd
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-10 05:31:24 +0900

    componentについて

--- dotinstall/vue/main.js
+++ dotinstall/vue/main.js
@@ -1,53 +1,19 @@
 (function(){
   'use strict';
 
-  var vn = new Vue({
-    el: '#app',
-    data: {
-      newItem: '',
-      todos: [],
-    },
-    watch: {
-      todos: {
-        handler: function(){
-          localStorage.setItem('todos', JSON.stringify(this.todos));
-        },
-        deep: true,
-      }
-    },
-    mounted: function(){
-      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-    },
-    methods: {
-      addItem: function() {
-        var item = {
-          title: this.newItem,
-          isDone: false,
-        };
-        this.todos.push(item);
-        this.newItem = '';
-      },
-      deleteItem: function(index) {
-        if(confirm('surely?')){
-          this.todos.splice(index, 1);
-        }
-      },
-      purge: function() {
-        if(!confirm('finished?')){
-          return;
-        }
-        this.todos = this.remaining;
-      }
-    },
-    computed: {
-      remaining: function() {
-        return this.todos.filter(function(todo){
-          return !todo.isDone;
-        });
-      }
-    }
+  var likeComponent = Vue.extend({
+    template: '<button>Like</button>',
   });
 
+  var app = new Vue({
+    el: '#app',
+    components: {
+      'like-component' : likeComponent,
+    }
+    // data: {
+      
+    // },
+  });
 
 })();
 
