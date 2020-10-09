1b1b932c0dbfe51dc5d8f33062b34e19dbf229b1
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-10 06:21:29 +0900

    todo配列作成

--- output/todo/main.js
+++ output/todo/main.js
@@ -1,7 +1,33 @@
-(function(){
-  'use strict';
-  
-  var app = new Vue({
+(function () {
+  "use strict";
 
+  const app = new Vue({
+    el: "#app",
+    data: {
+      todos: [
+        {
+          task: "ゴミ捨て",
+          date: '2020/10/09',
+          done: false,
+        },
+        {
+          task: "買い物",
+          date: '2020/07/13',
+          done: true,
+        },
+        {
+          task: "勉強",
+          date: '2020/10/13',
+          done: false,
+        },
+      ],
+    },
+    methods: {
+      // createDate: function(){
+      //   let d = new Date();
+      //   let date =  d.getFullYear() + "/" + d.getMonth()+ 1 + "/" + d.getDate();
+      //   this.todos.date = date;
+      // }
+    },
   });
 })();
