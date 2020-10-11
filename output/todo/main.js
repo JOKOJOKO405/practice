21ab31cea7a3f2f589343a50e90fcfadb7dcdc7a
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-11 16:20:25 +0900

    タスク修正機能

--- output/todo/main.js
+++ output/todo/main.js
@@ -8,25 +8,35 @@
       addItems: '',
       todos: [],
       modify: false,
-      modifyItmes: '',
+      taskIndex: '',
     },
     methods: {
       insertTodo: function(){
         let d = new Date();
         let date =  d.getFullYear() + "/" + (d.getMonth()+ 1) + "/" + d.getDate();
 
-        if(this.addItems !== ''){
-          var items = {
-            task: this.addItems,
-            done: false,
-            date: date,
-            modify: false,
+        if(!this.modify){
+          if(this.addItems !== ''){
+            var items = {
+              task: this.addItems,
+              done: false,
+              date: date,
+            }
+            this.todos.push(items);
+            this.addItems = '';
+          }else{
+            alert('タスクを入力してください');
           }
-          this.todos.push(items);
-          this.addItems = '';
         }else{
-          alert('タスクを入力してください');
+          if(this.addItems !== ''){
+            this.todos[this.taskIndex].task = this.addItems;
+            this.addItems = '';
+            this.modify = false;
+          }else{
+            alert('タスクを入力してください');
+          }
         }
+        
       },
       deleteTodo: function(index){
         if(!this.todos[index].done){
@@ -36,25 +46,10 @@
         }
       },
       modifyShow: function(index){
-
-        this.todos[index].modify = true;
-        this.modifyItems = this.todos[index].task;
+        this.modify = true;
+        this.addItems = this.todos[index].task;
+        this.taskIndex = index;
       },
-      modifyTodo: function(index){
-        if(this.modifyItems !== ''){
-          var items = {
-            task: this.modifyItems,
-            done: false,
-            date: this.todos[index].date,
-            modify: false,
-          }
-          this.index.map(newTodo => {
-            return items;
-          });
-        }else{
-          alert('タスクを入力してください');
-        }
-      }
     },
   });
 })();
